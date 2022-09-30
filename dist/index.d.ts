c6bf2ee0428600c863390435e688eb2c219ed300
Ruben Marcus <ruben@rubenmarcus.dev>
2022-09-30 07:00:50 +0100

    all

---
+++ dist/index.d.ts
@@ -0,0 +1,3 @@
+/// <reference types="react" />
+import type { JSX } from 'mintbase-widgets';
+export declare const MyNftCollection: import("react").ForwardRefExoticComponent<JSX.MyNftCollection & Omit<import("react").HTMLAttributes<HTMLMyNftCollectionElement>, "style"> & import("./react-component-lib/interfaces").StyleReactProps & import("react").RefAttributes<HTMLMyNftCollectionElement>>;
---
+++ dist/index.js
@@ -0,0 +1,8 @@
+/* eslint-disable */
+/* tslint:disable */
+/* auto-generated react proxies */
+import { createReactComponent } from './react-component-lib';
+import { applyPolyfills, defineCustomElements } from 'mintbase-widgets/loader';
+applyPolyfills().then(() => defineCustomElements());
+export const MyNftCollection = /*@__PURE__*/ createReactComponent('my-nft-collection');
+//# sourceMappingURL=index.js.map
